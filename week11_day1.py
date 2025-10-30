2fb1ba3b82959072323a861a87564427ff1c7968
sleepycat-glitch <asleepycat15@gmail.com>
2025-10-30 13:33:03 +0000

    online

---
+++ week11_day1.py
@@ -0,0 +1,41 @@
+# Day 2 Review Activity: My Personal Info Generator
+# ----------------------------------------
+
+print(" Welcome to the Python Review Activity!")
+print("You’ll review variables, strings, numbers, and print formatting.\n")
+
+# Step 1: Create Variables
+# TODO: Replace the values below with your own info
+first_name = "Ayelen"
+age = 17
+favorite_color = "red"
+favorite_number = 1
+
+#  Step 2: Practice String Operations
+# 1. Print your name in uppercase
+print(first_name.upper())
+
+# 2. Print how many letters are in your name
+print(len(first_name))
+
+# 3. Combine your name and favorite color into one message
+print(f"Hello! My name is {first_name}, and my favorite color is {favorite_color}.")
+
+#  Step 3: Math Practice
+# Use arithmetic operators with your favorite number
+new_age = age + favorite_number
+print(f"In April 2026, I will be {new_age} years old.")
+
+#  Step 4: User Input Practice
+# Ask the user two questions and combine answers
+age1(int(input("How old will you be next year?: ")))
+older(input("Are you older than me?: "))
+if age1 > age, older = True
+    else older = False
+print(f"You will be {age1} next year. Are you older than me?: {older}")
+
+
+
+# ⚙️ Step 5: Final Challenge (combine it all)
+# Use math and strings together
+
